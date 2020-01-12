6d798db64836892fc3c8e6e2fdebb69d722a6484
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-01-12 22:19:55 +0300

    Add input.py

---
+++ wifihunter/utils/input.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Fix for raw_input on python3: https://stackoverflow.com/a/7321970
+try:
+    input = raw_input
+except NameError:
+    pass
+
+raw_input = input
+
+try:
+    range = xrange
+except NameError:
+    pass
+
+xrange = range
