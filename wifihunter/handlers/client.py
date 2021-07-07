c456bcb4fb384a906cb9705ba335a309631d3782
Mustafa Asaad <ma24th@yahoo.com>
2021-07-08 02:41:05 +0300

    Update client.py

--- wifihunter/handlers/client.py
+++ wifihunter/handlers/client.py
@@ -34,10 +34,3 @@ class Client(object):
             result += key + ': ' + str(value)
             result += ', '
         return result
-
-
-if __name__ == '__main__':
-    fields = 'AA:BB:CC:DD:EE:FF, 2020-01-01 00:00:01, 2020-01-01 00:00:05, -67, 2, (not associated), HOME-ABCD'.split(
-        ',')
-    c = Client(fields)
-    print('Client', c)
