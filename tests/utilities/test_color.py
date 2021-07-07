6f1bbb0ae4ffda313539fa9787048f5a6bae1a52
Mustafa Asaad <ma24th@yahoo.com>
2021-07-08 01:51:10 +0300

    Update test_color.py

--- tests/utilities/test_color.py
+++ tests/utilities/test_color.py
@@ -2,8 +2,8 @@ import pytest
 from wifihunter.utilities.color import Color
 
 def test_color():
-    Color.pl('{R}Testing{G}One{C}Two{P}Three{W}Done') = ""
-    print(Color.s('{C}Testing{P}String{W}')) = ""
+    Color.pl('{R}Testing{G}One{C}Two{P}Three{W}Done')
+    print(Color.s('{C}Testing{P}String{W}'))
     Color.pl('{+} Good line')
     Color.pl('{!} Danger')
     
